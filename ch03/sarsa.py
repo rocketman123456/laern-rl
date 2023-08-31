ecc39a73cbc4fc6b241a1df9f0880d4f9ec3332e
rocketman123456 <759094438@qq.com>
2023-08-31 10:13:09 +0800

    add sarsa

---
+++ ch03/sarsa.py
@@ -0,0 +1,91 @@
+# Sarsa 直接估计 Q 表格，得到 Q 表格后，就可以更新策略。Q(st​,at​) = Q(st​,at​)+α[rt+1​+γQ(st+1​,at+1​)−Q(st​,at​)]
+import numpy as np
+from collections import defaultdict
+import torch
+import math
+class Sarsa(object):
+    def __init__(self, n_actions,cfg):
+        self.n_actions = n_actions  
+        self.lr = cfg.lr  
+        self.gamma = cfg.gamma  
+        self.sample_count = 0 
+        self.epsilon_start = cfg.epsilon_start
+        self.epsilon_end = cfg.epsilon_end
+        self.epsilon_decay = cfg.epsilon_decay 
+        self.Q  = defaultdict(lambda: np.zeros(n_actions)) # Q table
+    def sample(self, state):
+        self.sample_count += 1
+        self.epsilon = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * \
+            math.exp(-1. * self.sample_count / self.epsilon_decay) # The probability to select a random action, is is log decayed
+        best_action = np.argmax(self.Q[state])
+        action_probs = np.ones(self.n_actions, dtype=float) * self.epsilon / self.n_actions
+        action_probs[best_action] += (1.0 - self.epsilon)
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs) 
+        return action
+    def predict(self,state):
+        return np.argmax(self.Q[state])
+    def update(self, state, action, reward, next_state, next_action, terminated, truncated):
+        Q_predict = self.Q[state][action]
+        if terminated or truncated:
+            Q_target = reward  # 终止状态
+        else:
+            Q_target = reward + self.gamma * self.Q[next_state][next_action] # 与Q learning不同，Sarsa是拿下一步动作对应的Q值去更新
+        self.Q[state][action] += self.lr * (Q_target - Q_predict) 
+    def save(self,path):
+        '''把 Q表格 的数据保存到文件中
+        '''
+        import dill
+        torch.save(
+            obj=self.Q,
+            f=path+"sarsa_model.pkl",
+            pickle_module=dill
+        )
+    def load(self, path):
+        '''从文件中读取数据到 Q表格
+        '''
+        import dill
+        self.Q =torch.load(f=path+'sarsa_model.pkl',pickle_module=dill)
+
+def train(cfg,env,agent):
+    print('开始训练！')
+    print(f'环境:{cfg.env_name}, 算法:{cfg.algo_name}, 设备:{cfg.device}')
+    rewards = []  # 记录奖励
+    for i_ep in range(cfg.train_eps):
+        ep_reward = 0  # 记录每个回合的奖励
+        state = env.reset()  # 重置环境,即开始新的回合
+        action = agent.sample(state)
+        while True:
+            action = agent.sample(state)  # 根据算法采样一个动作
+            next_state, reward, terminated, truncated , _ = env.step(action)  # 与环境进行一次动作交互
+            next_action = agent.sample(next_state)
+            agent.update(state, action, reward, next_state, next_action, terminated, truncated) # 算法更新
+            state = next_state # 更新状态
+            action = next_action
+            ep_reward += reward
+            if terminated or truncated:
+                break
+        rewards.append(ep_reward)
+        print(f"回合：{i_ep+1}/{cfg.train_eps}，奖励：{ep_reward:.1f}，Epsilon：{agent.epsilon}")
+    print('完成训练！')
+    return {"rewards":rewards}
+
+def test(cfg,env,agent):
+    print('开始测试！')
+    print(f'环境：{cfg.env_name}, 算法：{cfg.algo_name}, 设备：{cfg.device}')
+    rewards = []  # 记录所有回合的奖励
+    for i_ep in range(cfg.test_eps):
+        ep_reward = 0  # 记录每个episode的reward
+        state = env.reset()  # 重置环境, 重新开一局（即开始新的一个回合）
+        while True:
+            action = agent.predict(state)  # 根据算法选择一个动作
+            next_state, reward, terminated, truncated, _ = env.step(action)  # 与环境进行一个交互
+            state = next_state  # 更新状态
+            ep_reward += reward
+            if terminated or truncated:
+                break
+        rewards.append(ep_reward)
+        print(f"回合数：{i_ep+1}/{cfg.test_eps}, 奖励：{ep_reward:.1f}")
+    print('完成测试！')
+    return {"rewards":rewards}
+
+
